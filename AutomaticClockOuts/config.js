52c1482f13daf0f1d2e13927229c681b7c2d639f
Rene Enriquez <enriquezrene.ap@gmail.com>
2020-11-09 00:47:52 -0500

    Introducing env vars in Azure Functions

--- AutomaticClockOuts/config.js
+++ AutomaticClockOuts/config.js
@@ -1,13 +1,13 @@
 const config = {
-    endpoint: "xxx",
-    key: "xxx",
+    endpoint: process.env["ENDPOINT"],
+    key: process.env["KEY"],
     databaseId: "time-tracker-db",
     containerId: "time_entry",
     partitionKey: { kind: "Hash", paths: ["/category"] },
-    clientId: "xxx",
-    authority: "xxx",
-    clientSecret: "xxx",
-    slackWebHook: "xxx"
+    clientId: process.env["CLIENT_ID"],
+    authority: process.env["AUTHORITY"],
+    clientSecret: process.env["CLIENT_SECRET"],
+    slackWebHook: process.env["SLACK_WEBHOOK"]
 };
 
 module.exports = config;
