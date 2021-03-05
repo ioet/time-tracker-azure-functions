5002cf08828f6e2a6c3f30dfe5b43b66d82b04c0
Israel Leon <israelpat42@gmail.com>
2021-03-04 21:22:30 -0500

    feat: TT-16 Tag people using their Slack nickname on Slack

--- nodejs-functions/src/handlers/automatic-clock-outs/clock_out.js
+++ nodejs-functions/src/handlers/automatic-clock-outs/clock_out.js
@@ -1,9 +1,11 @@
+const _ = require('lodash');
 const CosmosClient = require("@azure/cosmos").CosmosClient;
 const config = require("./config");
 const TimeEntry = require('./time_entry');
 const axios = require('axios');
-const MsalClient = require('./msal_client')
-const TimeEntryDao = require('./time_entry_dao')
+const MsalClient = require('./msal_client');
+const TimeEntryDao = require('./time_entry_dao');
+const SlackClient = require('./slack_client');
 
 const doClockOut = async (context) => {
   context.log(`I am going to check how many entries were not clocked out ${new Date()}`);
@@ -16,21 +18,27 @@ const doClockOut = async (context) => {
 
   const response = await MsalClient.findUsersInMS();
   const users = response.data.value;
+  const slackUsers = await SlackClient.findUsersInSlack();
+
   const {resources: entries} = await timeEntryDao.getEntriesWithNoEndDate();
   context.log(`Checking for time-entries that need to be clocked out`);
 
   let totalClockOutsExecuted = 0;
-  const usersWithClockOut = []
+  const usersWithClockOut = [];
   await Promise.all(entries.map(async (timeEntryAsJson) => {
     const timeEntry = new TimeEntry(timeEntryAsJson)
     if (timeEntry.needsToBeClockedOut()) {
-      usersWithClockOut.push(findUser(users, timeEntry.timeEntry.owner_id));
+      const user_email = findUserEmail(users, timeEntry.timeEntry.owner_id);
+      const userId = findSlackUserId(slackUsers,user_email);
+      if(userId){
+        usersWithClockOut.push("<@"+userId+">");
+      }
       timeEntryAsJson.end_date = timeEntry.getTimeToClockOut()
       await container.item(timeEntryAsJson.id, timeEntryAsJson.tenant_id).replace(timeEntryAsJson)
       totalClockOutsExecuted++
     }
   }));
-  if (totalClockOutsExecuted > 0) {
+  if (usersWithClockOut.length) {
     axios.post(slackWebHook,
       {
         "text": `OMG, you have been working more than 12 hours in a row. \nPlease take a break and visit https://timetracker.ioet.com/ to set the right end time for your entries, we just did a clock out for you :wink: \n- ${usersWithClockOut.join('\n- ')}`
@@ -46,9 +54,14 @@ const doClockOut = async (context) => {
   context.log(`I just clocked out ${totalClockOutsExecuted} entries, thanks are not needed...`);
 }
 
-const findUser = (users, id) => {
+const findUserEmail = (users, id) => {
   const user = users.find(user => user.objectId === id)
-  return user.displayName
+  return _.first(user.otherMails)
+}
+
+const findSlackUserId = (users,email)=>{
+  const user = users.find(user => user.email === email);
+  return user? user.id:null
 }
 
 module.exports = {doClockOut};
