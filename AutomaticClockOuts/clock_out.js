6bbfc1b6b9720229dc530510dcfe7043d6e3a970
Israel  Leon <israel.leon@unl.edu.ec>
2021-03-19 15:11:11 -0500

    feat: TT-184 Slack nick name send a personal message to users (#44)
    * feat: TT-184 Slack nick name send a personal message to users
    
    * feat: TT-184 Slack nick name send a personal message to users

--- AutomaticClockOuts/clock_out.js
+++ AutomaticClockOuts/clock_out.js
@@ -2,16 +2,16 @@ const _ = require('lodash');
 const CosmosClient = require("@azure/cosmos").CosmosClient;
 const config = require("./config");
 const TimeEntry = require('./time_entry');
-const axios = require('axios');
 const MsalClient = require('./msal_client');
 const TimeEntryDao = require('./time_entry_dao');
 const SlackClient = require('./slack_client');
+const { CLOCK_OUT_MESSAGE } = require('./constants');
 
 const doClockOut = async (context) => {
   context.log(`I am going to check how many entries were not clocked out ${new Date()}`);
 
-  const {endpoint, key, databaseId, slackWebHook} = config;
-  const client = new CosmosClient({endpoint, key});
+  const { endpoint, key, databaseId } = config;
+  const client = new CosmosClient({ endpoint, key });
   const database = client.database(databaseId);
   const container = database.container('time_entry');
   const timeEntryDao = new TimeEntryDao(database);
@@ -25,13 +25,15 @@ const doClockOut = async (context) => {
 
   let totalClockOutsExecuted = 0;
   const usersWithClockOut = [];
+
   await Promise.all(entries.map(async (timeEntryAsJson) => {
     const timeEntry = new TimeEntry(timeEntryAsJson)
     if (timeEntry.needsToBeClockedOut()) {
       const user_email = findUserEmail(users, timeEntry.timeEntry.owner_id);
-      const userId = findSlackUserId(slackUsers,user_email);
+      const userId = findSlackUserId(slackUsers, user_email);
       if(userId){
         usersWithClockOut.push("<@"+userId+">");
+        SlackClient.sendMessageToUser(userId, CLOCK_OUT_MESSAGE);
       }
       timeEntryAsJson.end_date = timeEntry.getTimeToClockOut()
       await container.item(timeEntryAsJson.id, timeEntryAsJson.tenant_id).replace(timeEntryAsJson)
@@ -39,17 +41,8 @@ const doClockOut = async (context) => {
     }
   }));
   if (usersWithClockOut.length) {
-    axios.post(slackWebHook,
-      {
-        "text": `OMG, you have been working more than 12 hours in a row. \nPlease take a break and visit https://timetracker.ioet.com/ to set the right end time for your entries, we just did a clock out for you :wink: \n- ${usersWithClockOut.join('\n- ')}`
-      }
-    )
-      .then(function (response) {
-        // console.log(response);
-      })
-      .catch(function (error) {
-        context.log(error);
-      });
+    const ClockOutMessageChannel=`${CLOCK_OUT_MESSAGE} \n- ${usersWithClockOut.join('\n- ')}`;
+    SlackClient.sendMessageToChannel(ClockOutMessageChannel);
   }
   context.log(`I just clocked out ${totalClockOutsExecuted} entries, thanks are not needed...`);
 }
@@ -59,9 +52,11 @@ const findUserEmail = (users, id) => {
   return _.first(user.otherMails)
 }
 
-const findSlackUserId = (users,email)=>{
+const findSlackUserId = (users, email) => {
   const user = users.find(user => user.email === email);
-  return user? user.id:null
+  return user ? user.id : null
 }
 
-module.exports = {doClockOut};
+doClockOut(console)
+
+module.exports = { doClockOut };
---
+++ nodejs-functions/src/handlers/automatic-clock-outs/constants.js
@@ -0,0 +1,5 @@
+const constants = {
+  CLOCK_OUT_MESSAGE : 'OMG, you have been working more than 12 hours in a row. \nPlease take a break and visit https://timetracker.ioet.com/ to set the right end time for your entries, we just did a clock out for you :wink:'
+};
+
+module.exports = constants;
