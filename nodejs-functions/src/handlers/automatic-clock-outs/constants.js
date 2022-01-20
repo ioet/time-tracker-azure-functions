8ac3ba72b708f270190e6604d295fa65917acb59
Sandro Castillo <blosomsam@hotmail.com>
2022-01-20 11:35:50 -0500

    fix: TT-482 add a new constant

--- nodejs-functions/src/handlers/automatic-clock-outs/constants.js
+++ nodejs-functions/src/handlers/automatic-clock-outs/constants.js
@@ -1,5 +1,6 @@
 const constants = {
-  CLOCK_OUT_MESSAGE : 'OMG, you have been working more than 12 hours in a row. \nPlease take a break and visit https://timetracker.ioet.com/ to set the right end time for your entries, we just did a clock out for you :wink:'
+  CLOCK_OUT_MESSAGE : 'OMG, you have been working more than 12 hours in a row. \nPlease take a break and visit https://timetracker.ioet.com/ to set the right end time for your entries, we just did a clock out for you :wink:',
+  CLOCK_OUT_MESSAGE_MIDNIGHT : 'Hey guys, I did a clock out for you. \nPlease visit https://timetracker.ioet.com/ and set the right end time for your entries :pls:'
 };
 
 module.exports = constants;
