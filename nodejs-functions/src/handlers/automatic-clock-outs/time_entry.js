8585603a51fad38c72673027b7e543d80e0d095a
cxcarvaj <cxcarvaj@espol.edu.ec>
2022-02-09 13:14:06 -0500

    Fix: Recieve a messsage after 12 hours

--- nodejs-functions/src/handlers/automatic-clock-outs/time_entry.js
+++ nodejs-functions/src/handlers/automatic-clock-outs/time_entry.js
@@ -14,10 +14,23 @@ class TimeEntry {
     return moment().utc().toISOString();
   }
 
+  getMidnightInTimeEntryZone(){
+    return moment(this.timeEntry.start_date).utc()
+        .subtract(this.timeEntry.timezone_offset, 'minutes').endOf('day')
+  }
+
+  getCurrentTimeInTimeEntryZone(){
+    return moment().utc().subtract(this.timeEntry.timezone_offset, 'minutes')
+  }
+
   needsToBeClockedOut() {
     const currentTimeInUTC = moment().utc()
     const minutesRunning = moment.duration(currentTimeInUTC.diff(this.getStartTimeInUTC())).asMinutes()
     return minutesRunning > 720;
+  }
+
+  needsToBeClockedOutMidnight(){
+    return this.getMidnightInTimeEntryZone().isBefore(this.getCurrentTimeInTimeEntryZone())
   }
 }
 
